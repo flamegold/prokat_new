f0ff700cdafcc74daf764998f4c6fee2e9714557
flamegold <118981562+flamegold@users.noreply.github.com>
2022-12-02 14:18:44 +0500

    edit

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -19,19 +19,19 @@
         <div class="flex flex-col justify-center items-center gap-5 relative z-10">
           <div class = "px-4 justify-between items-center rounded flex w-[100%] lg:w-[850px] bg-gradient-to-r from-[#168fea] to-[#0e5fca] text-white">
             <div class = "uppercase text-xl md:text-3xl lg:text-3xl font-meduim">Заточка сноуборда/лыж</div>
-            <div class = "font-meduim md:text-2xl lg:text-2xl font-bold w-[30%] text-right"><span class = "md:text-5xl lg:text-5xl">500</span> P</div>
+            <div class = "font-meduim md:text-2xl lg:text-2xl font-bold w-[30%] text-right"><span class = "md:text-5xl lg:text-5xl">600</span> P</div>
           </div>
           <div class = "px-4 justify-between items-center rounded flex w-[100%] lg:w-[850px] bg-gradient-to-r from-[#168fea] to-[#0e5fca] text-white">
             <div class = "uppercase text-xl md:text-3xl lg:text-3xl font-meduim">Парафинка сноуборда/лыж</div>
-            <div class = "font-meduim md:text-2xl lg:text-2xl font-bold w-[30%] text-right"><span class = "md:text-5xl lg:text-5xl">500</span> P</div>
+            <div class = "font-meduim md:text-2xl lg:text-2xl font-bold w-[30%] text-right"><span class = "md:text-5xl lg:text-5xl">600</span> P</div>
           </div>
           <div class = "px-4 justify-between items-center rounded flex w-[100%] lg:w-[850px] bg-gradient-to-r from-[#168fea] to-[#0e5fca] text-white">
             <div class = "uppercase text-xl md:text-3xl lg:text-3xl font-meduim">Комплекс (Парафин+заточка)</div>
-            <div class = "font-meduim md:text-2xl lg:text-2xl font-bold w-[30%] text-right"><span class = "md:text-5xl lg:text-5xl">900</span> P</div>
+            <div class = "font-meduim md:text-2xl lg:text-2xl font-bold w-[30%] text-right"><span class = "md:text-5xl lg:text-5xl">1100</span> P</div>
           </div>
           <div class = "px-4 justify-between items-center rounded flex w-[100%] lg:w-[850px] bg-gradient-to-r from-[#168fea] to-[#0e5fca] text-white">
             <div class = "uppercase text-xl md:text-3xl lg:text-3xl font-meduim">Ремонт скользящей поверхности</div>
-            <div class = "font-meduim md:text-2xl lg:text-2xl font-bold w-[30%] text-right">от <span class = "md:text-5xl lg:text-5xl">200</span> P</div>
+            <div class = "font-meduim md:text-2xl lg:text-2xl font-bold w-[30%] text-right">от <span class = "md:text-5xl lg:text-5xl">300</span> P</div>
           </div>
         </div>
       </div>
