07ba262a131ea277920152c2d4c4d910cc9c607e
flamegold <flamegolds@yandex.ru>
2022-12-13 11:03:03 +0500

    edit

---
+++ src/components/BlockInfo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="relative overflow-hidden">
+    <div
+      class="absolute lg:bg-[url('/img/U11.png')] w-[100%] bg-no-repeat h-[250px] bg-[length:400px_100%] top-[10%] left-[3%]">
+    </div>
+  </div>
+  <div class="bg-[url('/img/Fon1.jpg')] bg-center bg-no-repeat  bg-[length:100%_100%] text-white">
+    <div class="relative container mx-auto px-4 lg:py-20 pt-10 pb-[150px] z-10">
+      <div class="lg:grid lg:grid-cols-[230px_500px_1fr]">
+        <div class="lg:w-[230px]"></div>
+        <div class="lg:text-left text-center">
+          <p class="text-3xl lg:text-4xl font-medium leading-tight mb-5 uppercase">
+            Прокат сноубордов горных лыж
+          </p>
+          <p class="text-3xl lg:text-4xl font-medium leading-tight mb-5 uppercase">
+            Прокат и продажа снаряжения
+          </p>
+          <p class="text-3xl lg:text-4xl font-medium leading-tight uppercase mb-5">
+            Сервис
+          </p>
+          <p class="lg:text-xl text-lg font-medium leading-tight mb-2 lg:mb-0">
+            Адрес г.Тюмень, ул. Республики,137
+          </p>
+          <p class="lg:text-lg font-normal leading-tight">
+            (на пересечении Республики и Холодильной)
+          </p>
+          <p class="lg:text-lg font-normal leading-tight mb-5 lg:mb-10">
+            Вход со обратной стороны здания
+          </p>
+          <div class="border-2 w-[230px] text-center rounded py-3 lg:mx-0 mx-auto">
+            <p class="text-2xl font-normal leading-tight uppercase">
+              Время работы
+            </p>
+            <p class="text-xl font-normal leading-tight uppercase">
+              с 10:00 до 21:00
+            </p>
+          </div>
+        </div>
+        <div><img class="hidden lg:block w-[500px]" src="/img/36.png" /></div>
+      </div>
+    </div>
+  </div>
+  <div
+    class="absolute lg:bg-[url('/img/U2.png')] w-[300px] bg-no-repeat h-[356px] bg-[length:280px_100%] bottom-[35%] right-0">
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+
+</style>
